7e0549226e99bec7000894b0a74e4bb5b475a877
juaca2009 <juaca2009@gmail.com>
2021-09-29 13:36:52 -0500

    script entorno

---
+++ entorno.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+iniciar_entorno(){
+	
+	cd /home/juan/Documentos/"entornos python"/"feature selection"/bin
+	source activate
+	cd ../../../proyectos/"Trabajo de Grado"/FeatureSelecctionMusic
+}
+
+iniciar_entorno
