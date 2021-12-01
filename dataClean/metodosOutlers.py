26ea21d556591292c301f8081cb513aef92b81e1
juaca2009 <juaca2009@gmail.com>
2021-12-01 15:12:45 -0500

    eliminacion variable sin usar

--- dataClean/metodosOutlers.py
+++ dataClean/metodosOutlers.py
@@ -4,7 +4,6 @@ import numpy as np
 def reemplazarOutlersG(_data, _listaGaussianos):
     for i in _listaGaussianos:
         IQR = _data[i].quantile(0.75) - _data[i].quantile(0.25)
-        media = _data[i].mean()
         extremoInferior = _data[i].quantile(0.25) - (IQR*1.5)
         extremoSuperior = _data[i].quantile(0.75) + (IQR*1.5)
         _data.loc[_data[i]<=extremoInferior, i] = extremoInferior
