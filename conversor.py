69139bae40da444022ab70406aac02fb166192f9
juaca2009 <juaca2009@gmail.com>
2023-01-21 17:36:49 -0500

    Conversor con nombre.

--- conversor.py
+++ conversor.py
@@ -1,79 +1,82 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 
-class conversor:
+
+class Conversor:
     def __init__(self):
-        self.__dataFrame = None
+        self.__data_frame = None
         self.__outlers = {
-                'G': {'flt': [0.06129732355475424, 0.27289628610014904], 'lds': [-22.32607102394104, -1.3311007022857666],
-                      'strpk': [0.1641031801700592, 1.8732363283634186], 'flu': [0.04072224255651239, 0.12555399443954215],
-                      'entr': [6.43141233921051, 8.202026009559631], 'danc': [0.7248491942882538, 1.9346024692058563], 
-                      'bpm': [65.28873348236084, 181.0199728012085], 'ptch': [0.3889099434018135, 0.6416881754994392],
-                      'mfcc1': [-755.3796287500002, -560.6806187499999], 'mfcc2': [50.95847625000002, 187.92795425],
-                      'mfcc3': [-40.761771124999996, 55.675407875], 'mfcc4': [-8.994660624999998, 52.999048375]},
-                'NG': {'alds': [0.8655174970626831, 1.0372579097747803, 0.9295764177389566], 
-                       'nrg': [0.0016927169635891498, 0.056420412845909554, 0.030583320065380705]}
-                }
+            'G': {'flt': [0.06129732355475424, 0.27289628610014904], 'lds': [-22.32607102394104, -1.3311007022857666],
+                  'strpk': [0.1641031801700592, 1.8732363283634186], 'flu': [0.04072224255651239, 0.12555399443954215],
+                  'entr': [6.43141233921051, 8.202026009559631], 'danc': [0.7248491942882538, 1.9346024692058563],
+                  'bpm': [65.28873348236084, 181.0199728012085], 'ptch': [0.3889099434018135, 0.6416881754994392],
+                  'mfcc1': [-755.3796287500002, -560.6806187499999], 'mfcc2': [50.95847625000002, 187.92795425],
+                  'mfcc3': [-40.761771124999996, 55.675407875], 'mfcc4': [-8.994660624999998, 52.999048375]},
+            'NG': {'alds': [0.8655174970626831, 1.0372579097747803, 0.9295764177389566],
+                   'nrg': [0.0016927169635891498, 0.056420412845909554, 0.030583320065380705]}
+        }
         self.__escalar = {
-                'G': {'lds': [-12.00908887, 3.68078636], 'nrg': [0.02915877, 0.0095662],
-                      'flu': [0.08300337, 0.01653184], 'bpm': [122.91790398, 22.33771277],
-                      'ptch': [0.51488477, 0.04888783], 'mfcc3': [7.11287377, 19.10214552]},
-                'NG': {'flt': [0.06975416, 0.27421737], 'alds': [0.8681559, 0.99068403],
-                       'strpk': [0.15187809, 1.87806413], 'entr': [6.4390043, 8.19057977],
-                       'danc': [0.72345431, 1.93494983], 'mfcc1': [-756.60627375, -559.26528375], 
-                       'mfcc2': [50.49788188, 188.33292687], 'mfcc4': [-8.70278937, 52.49991762]}
-                }
+            'G': {'lds': [-12.00908887, 3.68078636], 'nrg': [0.02915877, 0.0095662],
+                  'flu': [0.08300337, 0.01653184], 'bpm': [122.91790398, 22.33771277],
+                  'ptch': [0.51488477, 0.04888783], 'mfcc3': [7.11287377, 19.10214552]},
+            'NG': {'flt': [0.06975416, 0.27421737], 'alds': [0.8681559, 0.99068403],
+                   'strpk': [0.15187809, 1.87806413], 'entr': [6.4390043, 8.19057977],
+                   'danc': [0.72345431, 1.93494983], 'mfcc1': [-756.60627375, -559.26528375],
+                   'mfcc2': [50.49788188, 188.33292687], 'mfcc4': [-8.70278937, 52.49991762]}
+        }
 
-    def analizarOutlers(self):
-        columnasGaussianas = self.__outlers['G'].keys()
-        columnasNoGaussianas = self.__outlers['NG'].keys()
-        for i in columnasGaussianas:
-            valorTemp = self.__dataFrame[i].values
-            valorTemp = valorTemp[0]
-            if valorTemp < self.__outlers['G'][i][0]:
-                self.__dataFrame[i] = self.__outlers['G'][i][0]
-            elif valorTemp > self.__outlers['G'][i][1]:
-                self.__dataFrame[i] = self.__outlers['G'][i][1]
-        for i in columnasNoGaussianas:
-            valorTemp = self.__dataFrame[i].values
-            valorTemp = valorTemp[0]
-            if valorTemp < self.__outlers['NG'][i][0]:
-                self.__dataFrame[i] = self.__outlers['NG'][i][2]
-            elif valorTemp > self.__outlers['NG'][i][1]:
-                self.__dataFrame[i] = self.__outlers['NG'][i][2]
+    def analizar_outlers(self):
+        columnas_gaussianas = self.__outlers['G'].keys()
+        columnas_no_gaussianas = self.__outlers['NG'].keys()
+        for i in columnas_gaussianas:
+            valor_temp = self.__data_frame[i].values
+            valor_temp = valor_temp[0]
+            if valor_temp < self.__outlers['G'][i][0]:
+                self.__data_frame[i] = self.__outlers['G'][i][0]
+            elif valor_temp > self.__outlers['G'][i][1]:
+                self.__data_frame[i] = self.__outlers['G'][i][1]
+        for i in columnas_no_gaussianas:
+            valor_temp = self.__data_frame[i].values
+            valor_temp = valor_temp[0]
+            if valor_temp < self.__outlers['NG'][i][0] or valor_temp > self.__outlers['NG'][i][1]:
+                self.__data_frame[i] = self.__outlers['NG'][i][2]
 
-    def escalarValores(self):
-        columnasGaussianas = self.__escalar['G'].keys()
-        columnasNoGaussianas = self.__escalar['NG'].keys()
-        for i in columnasGaussianas:
-            valorTemp = self.__dataFrame[i].values
-            valorTemp = valorTemp[0]
-            self.__dataFrame[i] = (valorTemp-self.__escalar['G'][i][0])/self.__escalar['G'][i][1]
-        for i in columnasNoGaussianas:
-            valorTemp = self.__dataFrame[i].values
-            valorTemp = valorTemp[0]
-            self.__dataFrame[i] = (valorTemp-self.__escalar['NG'][i][0])/(self.__escalar['NG'][i][1]-self.__escalar['NG'][i][0])
+    def escalar_valores(self):
+        columnas_gaussianas = self.__escalar['G'].keys()
+        columnas_no_gaussianas = self.__escalar['NG'].keys()
+        for i in columnas_gaussianas:
+            valor_temp = self.__data_frame[i].values
+            valor_temp = valor_temp[0]
+            self.__data_frame[i] = (valor_temp - self.__escalar['G'][i][0]) / self.__escalar['G'][i][1]
+        for i in columnas_no_gaussianas:
+            valor_temp = self.__data_frame[i].values
+            valor_temp = valor_temp[0]
+            self.__data_frame[i] = (valor_temp - self.__escalar['NG'][i][0]) / (
+                        self.__escalar['NG'][i][1] - self.__escalar['NG'][i][0])
 
-    def transformarEntrada(self, _dataFrame):
-        self.setDataFrame(_dataFrame)
-        self.analizarOutlers()
-        self.escalarValores()
-        
-    def setDataFrame(self, _dataFrame):
-        self.__dataFrame = _dataFrame
+    def transformar_entrada(self, _data_frame):
+        sin_nombre = _data_frame.drop(['nombre'], axis=1)
+        self.set_dataframe(sin_nombre)
+        self.analizar_outlers()
+        self.escalar_valores()
+        self.__data_frame.insert(0, "nombre", _data_frame["nombre"].values[0])
 
-    def getDataFarame(self):
-        return self.__dataFrame
+    def set_dataframe(self, _data_frame):
+        self.__data_frame = _data_frame
+
+    def get_dataframe(self):
+        return self.__data_frame
+
 
 if __name__ == '__main__':
-    columnas = ['flt', 'lds', 'alds', 'strpk', 'nrg', 'flu', 'entr', 'danc', 'bpm',
+    columnas = ['nombre', 'flt', 'lds', 'alds', 'strpk', 'nrg', 'flu', 'entr', 'danc', 'bpm',
                 'ptch', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4']
-    datosPrueba = [[0.240082, -14.933464,  0.897602,  1.146760,  0.022006,  0.071873,  7.002771,  1.288145,  114.862709, 
-                    0.419456, -723.15356,  104.381910,  13.517786,  49.038660]]
+    datosPrueba = [["olvidala", 0.240082, -14.933464, 0.897602, 1.146760, 0.022006, 0.071873, 7.002771, 1.288145, 114.862709,
+                    0.419456, -723.15356, 104.381910, 13.517786, 49.038660]]
     df = pd.DataFrame(datosPrueba, columns=columnas)
     print(df)
-    a = conversor()
-    a.transformarEntrada(df)
-    df = a.getDataFarame()
+    a = Conversor()
+    a.transformar_entrada(df)
+    df = a.get_dataframe()
+    print("Resultado final")
     print(df)
-    print(df[['entr', 'danc']])
